9c25dfde70baf4a4918c72e11e59ece0fa1ca09b
0136C <132276936+0136C@users.noreply.github.com>
2026-05-03 04:25:11 +0300

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,5 @@
+city = 'Amsterdam'
+work = 'School 12'
+group = 'Group Work 1'
+worker = 'Worker'
+print(city,work,group, sep=' | ',end ='')
---
+++ 04.py
@@ -0,0 +1,10 @@
+name = "Vladislav"
+group = "Group 1"
+age = "18"
+city = "Moscow"
+print('+' + '-' * 28 + '+')
+print(f'| NAME : {name}' + ' ' * (20 - len(name)) + '|')
+print(f'| GROUP: {group}' + ' ' * (20 - len(group)) + '|')
+print(f'| AGE  : {age}' + ' ' * (20 - len(age)) + '|')
+print(f'| CITY : {city}' + ' ' * (20 - len(city)) + '|')
+print('+' + '-' * 28 + '+')
